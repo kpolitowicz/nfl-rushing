c12cea77bb7a6a8fce4943128f9abaca877f83e4
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-27 17:46:13 -0500

    reading all stats from the file

--- lib/nfl_rushing/player_stats.ex
+++ lib/nfl_rushing/player_stats.ex
@@ -5,9 +5,34 @@ defmodule NflRushing.PlayerStats do
 
   alias NflRushing.Player
 
+  @doc """
+  Returns a list of all players as read from the JSON file.
+  """
   def all do
-    [
-      %Player{name: "Joe Banyard"}
-    ]
+    {:ok, stats_str} = File.read(__DIR__ <> "/../../rushing.json")
+
+    stats_str
+    |> Jason.decode!()
+    |> Enum.map(fn attrs -> to_player(attrs) end)
+  end
+
+  defp to_player(attrs) do
+    %Player{
+      name: attrs["Player"],
+      team: attrs["Team"],
+      position: attrs["Pos"],
+      rushing_attempts: attrs["Att"],
+      rushing_attempts_per_game: attrs["Att/G"],
+      total_rushing_yards: attrs["Yds"],
+      avg_rushing_yards_per_attempt: attrs["Avg"],
+      rushing_yards_per_game: attrs["Yds/G"],
+      total_rushing_touchdowns: attrs["TD"],
+      longest_rush: attrs["Lng"],
+      rushing_first_downs: attrs["1st"],
+      rushing_first_down_percentage: attrs["1st%"],
+      rushing_20_plus_yards: attrs["20+"],
+      rushing_40_plus_yards: attrs["40+"],
+      rushing_fumbles: attrs["FUM"]
+    }
   end
 end
