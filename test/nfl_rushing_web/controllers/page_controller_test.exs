55f1eee5d51398fde90c8106f2996a7ba6b0ffdb
Kamil Politowicz <k.politowicz@gmail.com>
2019-12-28 08:39:29 -0500

    using Floki to better test the table

--- test/nfl_rushing_web/controllers/page_controller_test.exs
+++ test/nfl_rushing_web/controllers/page_controller_test.exs
@@ -2,24 +2,32 @@ defmodule NflRushingWeb.PageControllerTest do
   use NflRushingWeb.ConnCase
 
   describe "GET /" do
-    test "shows the page title", %{conn: conn} do
-      conn = get(conn, "/")
-
-      assert html_response(conn, 200) =~ "NFL players' rushing statistics"
-      assert html_response(conn, 200) =~ "Current temperature: 31"
-    end
-
     test "shows the players' stats table", %{conn: conn} do
       conn = get(conn, "/")
       resp = html_response(conn, 200)
 
-      assert resp =~ "<td>Joe Banyard</td>"
+      assert resp =~ "NFL players' rushing statistics"
+      assert players_count(resp) == 326
+      assert first_player_name(resp) == "Joe Banyard"
     end
 
     test "changes temperature to 40", %{conn: conn} do
       {:ok, view, _html} = live(conn, "/")
 
       assert render_click(view, :change_temperature) =~ "Current temperature: 40"
+    end
+
+    defp players_count(html) do
+      html
+      |> Floki.find("table tbody tr")
+      |> length
+    end
+
+    defp first_player_name(html) do
+      html
+      |> Floki.find("table tbody tr td")
+      |> hd
+      |> Floki.text
     end
   end
 end
